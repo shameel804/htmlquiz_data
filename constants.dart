f0e8736c6f1bf500dc63a48eb506887fc510f8fb
shameel804 <shameeabid2@gmail.com>
2026-01-11 04:29:52 +0530

    updated learn

---
+++ constants.dart
@@ -0,0 +1,193 @@
+class Constants {
+  static const Map<String, List<Map<String, dynamic>>> allTypes = {
+    "Html": [
+      {
+        'id': 'basic',
+        'title': 'Basics',
+        'tier': 'free',
+        'topics': ["Tags", "elements", "attributes", "structure"],
+      },
+      {
+        'id': 'forms',
+        'title': 'Forms',
+        'tier': 'free',
+        'topics': ["Form elements", "inputs", "buttons", "validation"],
+      },
+      {
+        'id': 'html5',
+        'title': 'HTML5 New Features',
+        'tier': 'free',
+        'topics': ["Semantic tags", "multimedia (audio/video)", "canvas"],
+      },
+      {
+        'id': 'tables',
+        'title': 'Tables',
+        'tier': 'free',
+        'topics': ["Table structure", "headers", "captions", "colspan/rowspan"],
+      },
+      {
+        'id': 'lists',
+        'title': 'Lists',
+        'tier': 'free',
+        'topics': ["Ordered", "unordered", "description lists"],
+      },
+      {
+        'id': 'links',
+        'title': 'Links & Navigation',
+        'tier': 'vip',
+        'topics': ["Anchor tags", "navigation menus"],
+      },
+      {
+        'id': 'media',
+        'title': 'Media Elements',
+        'tier': 'vip',
+        'topics': ["Embedding videos", "audio", "images"],
+      },
+    ],
+    "Css": [
+      {
+        'id': 'basics',
+        'title': 'Basics',
+        'tier': 'free',
+        'topics': ["Selectors", "properties", "values", "syntax"],
+      },
+      {
+        'id': 'box',
+        'title': 'Box Model',
+        'tier': 'free',
+        'topics': ["Margin", "border", "padding", "content"],
+      },
+      {
+        'id': 'position',
+        'title': 'Positioning',
+        'tier': 'free',
+        'topics': ["Static", "relative", "absolute", "fixed", "sticky"],
+      },
+      {
+        'id': 'flex',
+        'title': 'Flexbox & Grid',
+        'tier': 'free',
+        'topics': [
+          "Containers",
+          "flex items",
+          "alignment",
+          "spacing",
+          "Grid containers",
+          "tracks",
+          "areas",
+        ],
+      },
+      {
+        'id': 'font',
+        'title': 'Typography',
+        'tier': 'free',
+        'topics': ["Fonts", "text alignment", "line height", "letter spacing"],
+      },
+      {
+        'id': 'color',
+        'title': 'Colors and Backgrounds',
+        'tier': 'free',
+        'topics': ["Color formats", "gradients", "background images"],
+      },
+      {
+        'id': 'anim',
+        'title': 'Transitions & Animations',
+        'tier': 'vip',
+        'topics': ["Keyframes", "easing", "delays"],
+      },
+      {
+        'id': 'query',
+        'title': 'Media Queries',
+        'tier': 'vip',
+        'topics': ["Responsive design", "breakpoints"],
+      },
+      {
+        'id': 'pseudo',
+        'title': 'Pseudo-classes & Elements',
+        'tier': 'vip',
+        'topics': ["Hover effects", "first-child", "::before", "::after"],
+      },
+    ],
+    "Javascript": [
+      {
+        'id': 'basic',
+        'title': 'Basics',
+        'tier': 'free',
+        'topics': ["Variables", "data types", "operators", "syntax"],
+      },
+      {
+        'id': 'control',
+        'title': 'Control Structures',
+        'tier': 'free',
+        'topics': ["If-else", "switch-case", "loops (for, while, do-while)"],
+      },
+      {
+        'id': 'func',
+        'title': 'Functions',
+        'tier': 'free',
+        'topics': [
+          "Function declarations",
+          "expressions",
+          "arrow functions",
+          "scopes",
+        ],
+      },
+      {
+        'id': 'dom',
+        'title': 'DOM Manipulation',
+        'tier': 'free',
+        'topics': [
+          "Selecting elements",
+          "event handling",
+          "modifying HTML/CSS",
+        ],
+      },
+      {
+        'id': 'events',
+        'title': 'Events',
+        'tier': 'free',
+        'topics': [
+          "Event listeners",
+          "bubbling",
+          "delegation",
+          "default actions",
+        ],
+      },
+      {
+        'id': 'obj',
+        'title': 'Objects and Arrays',
+        'tier': 'free',
+        'topics': ["Creation", "methods", "destructuring"],
+      },
+      {
+        'id': 'es6',
+        'title': 'ES6+ Features',
+        'tier': 'vip',
+        'topics': [
+          "Let/const",
+          "template literals",
+          "spread/rest operators",
+          "modules",
+        ],
+      },
+      {
+        'id': 'async',
+        'title': 'Asynchronous JavaScript',
+        'tier': 'vip',
+        'topics': ["Callbacks", "promises", "async/await"],
+      },
+      {
+        'id': 'error',
+        'title': 'Error Handling',
+        'tier': 'vip',
+        'topics': ["Try-catch", "custom errors", "debugging"],
+      },
+      {
+        'id': 'api',
+        'title': 'APIs',
+        'tier': 'vip',
+        'topics': ["Fetch API", "local storage", "geolocation"],
+      },
+    ],
+  };
+}
